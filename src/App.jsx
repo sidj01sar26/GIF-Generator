631e3a26e79293a6381fd61f3e436e60e2e8af85
Siddharth Joisar <sidjoisar0807@gmail.com>
2024-02-27 01:54:54 +0530

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import Random from "./components/Random"
+import Tag from "./components/Tag";
+
 export default function App() {
-  return <div>Random GIF</div>;
+  return (
+    <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center">
+
+      <h1 className=" bg-black text-white rounded-lg w-11/12 text-center mt-[36px]
+       px-10 py-2 text-2xl font-bold ">
+        RANDOM GIFS
+      </h1>
+
+      <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
+        <Random />
+        <Tag />
+      </div>
+
+    </div>
+  );
 }
