ff2d2d64159a05c858c37267bbcbb93a158f482a
Siddharth Joisar <sidjoisar0807@gmail.com>
2024-02-27 23:34:58 +0530

    responsive

--- src/components/Random.js
+++ src/components/Random.js
@@ -17,7 +17,7 @@ const Random = () => {
       <h1 className='mt-[15px] text-2xl underline uppercase font-bold'>A Random Gif</h1>
 
       {
-        loading ? (<Spinner />) : (<img src={gif} width="450" alt='gif' />)
+        loading ? (<Spinner />) : (<img src={gif} className='imgif' width="570" alt='gif' />)
       }
 
       <button onClick={() => fetchData()}
